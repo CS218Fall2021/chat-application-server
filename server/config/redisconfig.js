33ffb0371f23d8054b78b60b016110ddf800c076
Rushikesh Padia <padiarushi3012@gmail.com>
2021-11-21 21:17:45 -0800

    bug fixes

--- server/config/redisconfig.js
+++ server/config/redisconfig.js
@@ -1,2 +1,2 @@
-const redis_endpoint = process.env.REDIS_ADDRESS || 'redis-name-ro.q9bsl4.ng.0001.use1.cache.amazonaws.com:6379'
+const redis_endpoint = 'redis-name-ro.q9bsl4.ng.0001.use1.cache.amazonaws.com:6379'
 exports.redis_endpoint = redis_endpoint
