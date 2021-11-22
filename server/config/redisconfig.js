c8b5cff09727e840c51904ac2d4a80be8ac9d020
Saurabh Kale <saurabhkale21@gmail.com>
2021-11-21 23:17:54 -0800

    Redis Endpoint

--- server/config/redisconfig.js
+++ server/config/redisconfig.js
@@ -1,2 +1,2 @@
-const redis_endpoint = 'redis-name-ro.q9bsl4.ng.0001.use1.cache.amazonaws.com:6379'
+const redis_endpoint = 'redis://redis-name-ro.q9bsl4.ng.0001.use1.cache.amazonaws.com:6379'
 exports.redis_endpoint = redis_endpoint
